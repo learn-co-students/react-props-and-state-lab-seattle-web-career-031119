11cc18fdbf1edf38c844a352950820d2534cf115
ConditionalStatementLifestyle <github email address>
2019-05-14 16:53:43 -0700

    Done.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,7 +4,14 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+    return (
+      <div className="ui cards">
+        {this.props.petData.map(pet => {
+          return <Pet key={pet.id} petInfo={pet} onAdoptPet={this.props.onAdoptPet}/>
+          })
+        }
+      </div>
+    )
   }
 }
 
